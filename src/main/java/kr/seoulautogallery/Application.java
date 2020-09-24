080b675d026000ac31b872e64ddd376607dd693d
CSLab1228 <chief_hi@naver.com>
2020-09-24 15:46:44 +0900

    카카오 로그인 미해결 / 팝업 추가 중 / 파일용량추가

--- src/main/java/kr/seoulautogallery/Application.java
+++ src/main/java/kr/seoulautogallery/Application.java
@@ -2,12 +2,28 @@ package kr.seoulautogallery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
 
 // @EnableJpaAuditing
+@ServletComponentScan
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofBytes(512000000L));
+        factory.setMaxRequestSize(DataSize.ofBytes(512000000L));
+        return factory.createMultipartConfig();
+    }
+
 }
